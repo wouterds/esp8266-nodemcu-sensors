fc97f4efb7bb9c2e0babd28518a71a7bee85bbf2
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-01-30 23:55:01 +0100

    :sparkles: WiFi setup

--- esp8266.ino
+++ esp8266.ino
@@ -1,12 +1,37 @@
+#include <ESP8266WiFi.h>
+
+// WiFi configuration
+const char *ssid = "";
+const char *password = "";
+
 void setup()
 {
   // Setup Serial
   Serial.begin(9600);
   Serial.println();
 
-  Serial.println("Hello world!");
+  setupWiFi();
 }
 
 void loop()
 {
+}
+
+void setupWiFi()
+{
+  Serial.println("[WiFi] Setup");
+  Serial.print("[WiFi] Connecting to: ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(200);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("[WiFi] Connected!");
+  Serial.print("[WiFi] IP: ");
+  Serial.println(WiFi.localIP());
 }
